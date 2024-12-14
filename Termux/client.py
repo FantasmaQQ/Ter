b5c08b6b99b9117282386409fe283c3748fa317c
FantasmaQQ <141361091+FantasmaQQ@users.noreply.github.com>
2024-12-14 10:33:47 -0300

    Update client.py

--- Termux/client.py
+++ Termux/client.py
@@ -11,9 +11,11 @@ def receive_messages():
             client.close()
             break
 
-server_ip = input("Digite o IP do servidor: ")  # IP do servidor
+server_ip = input("Digite o IP do servidor: ")  # Insira o IP do servidor (ngrok)
+server_port = int(input("Digite a porta do servidor: "))  # Insira a porta do servidor (ngrok)
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((server_ip, 9999))  # Porta deve ser a mesma do servidor
+client.connect((server_ip, server_port))
 
 threading.Thread(target=receive_messages).start()
 
