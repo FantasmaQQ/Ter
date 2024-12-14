0967495d524dccc08c827e568f0204da3a0ee125
FantasmaQQ <141361091+FantasmaQQ@users.noreply.github.com>
2024-12-14 10:07:55 -0300

    Update client.py

--- Termux/client.py
+++ Termux/client.py
@@ -11,12 +11,13 @@ def receive_messages():
             client.close()
             break
 
-server_ip = input("Digite o IP do servidor: ")
+server_ip = input("Digite o IP do servidor (ou localhost para testes locais): ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((server_ip, 9999))
 
 threading.Thread(target=receive_messages).start()
 
+print("Conectado ao chat! Envie mensagens:")
 while True:
     message = input()
     client.send(message.encode('utf-8'))
